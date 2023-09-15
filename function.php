429e27f60167a9363bb8fe58d6bf94b81a55b82a
Silvia <panna.silvia@outlook.it>
2023-09-15 11:51:55 +0200

    Aggiunta funzione divisione con controllo

--- function.php
+++ function.php
@@ -18,4 +18,13 @@ function moltiplicazione (n1,n2) {
     return n1*n2;
 }
 
+/* divisione */
+
+function divisione (n1,n2) {
+    if (n2 !== 0)
+    return n1/n2;
+} else {
+    return "Error";
+}
+
 ?>
